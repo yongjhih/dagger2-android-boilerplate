0c4665c752c5cd84e4439c59e6176ad769967e1e
Andrew Chen <yongjhih@gmail.com>
2016-11-13 05:49:48 +0800

    Support rxpresso

--- app/src/androidTest/java/com/github/yongjhih/dagger2/sample/MainActivityTest.java
+++ app/src/androidTest/java/com/github/yongjhih/dagger2/sample/MainActivityTest.java
@@ -2,9 +2,12 @@ package com.github.yongjhih.dagger2.sample;
 
 import android.app.Instrumentation;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.filters.LargeTest;
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+
+import com.novoda.rxpresso.RxPresso;
+import com.novoda.rxpresso.matcher.RxMatcher;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -12,23 +15,22 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.Arrays;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import rx.Notification;
 import rx.Observable;
 
-import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
-import static mocker.Mocker.mocker;
-
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.mockito.Mockito.mock;
+import static com.novoda.rxpresso.matcher.RxExpect.any;
 import static org.mockito.Mockito.when;
 
 /**
@@ -51,19 +53,19 @@ public class MainActivityTest {
     @Rule
     public FragmentTestRule<ReposFragment> mFragmentRule = new FragmentTestRule<>(ReposFragment.class);
 
-    @Test
-    public void testListDisplayed() {
-        // Launch the activity to make the fragment visible
-        mFragmentRule.launchActivity(null);
-        onView(withId(R.id.list)).check(matches(isDisplayed()));
-    }
-
     @Before
     public void setUp() {
         Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
         App app = (App) instrumentation.getTargetContext().getApplicationContext();
         TestComponent component = (TestComponent) app.mainComponent();
         component.inject(this);
+    }
+
+    @Test
+    public void testListDisplayed() {
+        // Launch the activity to make the fragment visible
+        mFragmentRule.launchActivity(null);
+        onView(withId(R.id.list)).check(matches(isDisplayed()));
     }
 
     @Test
@@ -78,4 +80,21 @@ public class MainActivityTest {
         mFragmentRule.launchActivity(null);
         onView(withText("android-proguards")).check(matches(isDisplayed()));
     }
+
+    @Test
+    public void testRxRepos() {
+        RxPresso rxpresso = RxPresso.from(github);
+        Espresso.registerIdlingResources(rxpresso);
+        Repo repo = new Repo();
+        repo.owner.login = "yongjhh";
+        repo.owner.login = "https://avatars.githubusercontent.com/u/213736?v=3";
+        repo.name = "android-proguards";
+
+        rxpresso.given(github.repos("yongjhih").flatMap(Observable::from))
+                .withEventsFrom(Observable.just(repo))
+                .expect(any(Repo.class))
+                .thenOnView(withText("android-proguards"))
+                .check(matches(isDisplayed()));
+    }
+
 }
